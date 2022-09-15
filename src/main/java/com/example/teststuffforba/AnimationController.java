7080054ece8b33ed5041b2a30209c95e793b4d4c
Cedesc <christiandohmen1910@gmail.com>
2022-09-15 13:52:52 +0200

    more animations

--- src/main/java/com/example/teststuffforba/AnimationController.java
+++ src/main/java/com/example/teststuffforba/AnimationController.java
@@ -1,6 +1,6 @@
 package com.example.teststuffforba;
 
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -20,21 +20,79 @@ public class AnimationController implements Initializable {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        letsRotate();
     }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        simpleAnimation();
+    }
 
+    public void simpleAnimation() {
         // translate
         TranslateTransition translate = new TranslateTransition();
         translate.setNode(welcomeText);
-        translate.setDuration(Duration.millis(1000));
-        translate.setCycleCount(3);
+        translate.setDuration(Duration.millis(500));
+        translate.setCycleCount(4);
         translate.setByX(100);
         translate.setAutoReverse(true);
         translate.play();
-
     }
 
+    public void goCrazy() {
+
+        letsTranslate();
+
+        letsRotate();
+
+        letsFade();
+
+        letsScale();
+    }
+
+    public void letsTranslate() {
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(welcomeText);
+        translate.setDuration(Duration.millis(500));
+        translate.setCycleCount(TranslateTransition.INDEFINITE);
+        translate.setByX(100);
+        translate.setByY(100);
+        translate.setAutoReverse(true);
+        translate.play();
+    }
+
+    public void letsRotate() {
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(welcomeText);
+        rotate.setDuration(Duration.millis(1000));
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setInterpolator(Interpolator.LINEAR);
+
+        rotate.setByAngle(360);
+        rotate.play();
+    }
+
+    public void letsFade() {
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(welcomeText);
+        fade.setDuration(Duration.millis(1000));
+        fade.setCycleCount(TranslateTransition.INDEFINITE);
+        fade.setInterpolator(Interpolator.LINEAR);
+        fade.setFromValue(1);
+        fade.setToValue(0);
+        fade.play();
+    }
+
+    public void letsScale() {
+        ScaleTransition scale = new ScaleTransition();
+        scale.setNode(welcomeText);
+        scale.setDuration(Duration.millis(15000));
+        scale.setCycleCount(TranslateTransition.INDEFINITE);
+        scale.setInterpolator(Interpolator.LINEAR);
+        scale.setByX(15.0);
+        scale.setByY(15.0);
+        scale.setAutoReverse(true);
+        scale.play();
+    }
 
 }
