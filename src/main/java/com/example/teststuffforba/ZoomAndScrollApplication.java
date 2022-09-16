b5b7875f6efc2f072cfffa63375e91002428af60
cdohmen <cdohmen>
2022-09-16 17:58:08 +0200

    Alle nicht-scroll Klassen und Funktionen auskommentiert, sodass ich mich aufs scrollen konzentrieren kann

--- src/main/java/com/example/teststuffforba/ZoomAndScrollApplication.java
+++ src/main/java/com/example/teststuffforba/ZoomAndScrollApplication.java
@@ -1,18 +1,21 @@
 package com.example.teststuffforba;
 
 
-import com.example.teststuffforba.zooming.NodeGestures;
+//import com.example.teststuffforba.zooming.NodeGestures;
 import com.example.teststuffforba.zooming.PannableCanvas;
 import com.example.teststuffforba.zooming.SceneGestures;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+//import javafx.scene.control.Label;
+//import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+//import javafx.scene.paint.Color;
+//import javafx.scene.shape.Circle;
+//import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -35,45 +38,45 @@ public class ZoomAndScrollApplication extends Application {
 
         // we don't want the canvas on the top/left in this example => just
         // translate it a bit
-        canvas.setTranslateX(100);
-        canvas.setTranslateY(100);
+//        canvas.setTranslateX(100);
+//        canvas.setTranslateY(100);
 
         // create sample nodes which can be dragged
-        NodeGestures nodeGestures = new NodeGestures(canvas);
+//        NodeGestures nodeGestures = new NodeGestures(canvas);
 
-        Label label1 = new Label("Draggable node 1");
-        label1.setTranslateX(10);
-        label1.setTranslateY(10);
-        label1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
-        label1.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
+//        Label label1 = new Label("Draggable node 1");
+//        label1.setTranslateX(10);
+//        label1.setTranslateY(10);
+//        label1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
+//        label1.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
 
-        Label label2 = new Label("Draggable node 2");
-        label2.setTranslateX(100);
-        label2.setTranslateY(100);
-        label2.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
-        label2.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
+//        Label label2 = new Label("Draggable node 2");
+//        label2.setTranslateX(100);
+//        label2.setTranslateY(100);
+//        label2.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
+//        label2.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
 
-        Label label3 = new Label("Draggable node 3");
-        label3.setTranslateX(200);
-        label3.setTranslateY(200);
-        label3.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
-        label3.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
+//        Label label3 = new Label("Draggable node 3");
+//        label3.setTranslateX(200);
+//        label3.setTranslateY(200);
+//        label3.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
+//        label3.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
 
-        Circle circle1 = new Circle(300, 300, 50);
-        circle1.setStroke(Color.ORANGE);
-        circle1.setFill(Color.ORANGE.deriveColor(1, 1, 1, 0.5));
-        circle1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
-        circle1.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
+//        Circle circle1 = new Circle(300, 300, 50);
+//        circle1.setStroke(Color.ORANGE);
+//        circle1.setFill(Color.ORANGE.deriveColor(1, 1, 1, 0.5));
+//        circle1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
+//        circle1.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
+//
+//        Rectangle rect1 = new Rectangle(100, 100);
+//        rect1.setTranslateX(450);
+//        rect1.setTranslateY(450);
+//        rect1.setStroke(Color.BLUE);
+//        rect1.setFill(Color.BLUE.deriveColor(1, 1, 1, 0.5));
+//        rect1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
+//        rect1.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
 
-        Rectangle rect1 = new Rectangle(100, 100);
-        rect1.setTranslateX(450);
-        rect1.setTranslateY(450);
-        rect1.setStroke(Color.BLUE);
-        rect1.setFill(Color.BLUE.deriveColor(1, 1, 1, 0.5));
-        rect1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeGestures.getOnMousePressedEventHandler());
-        rect1.addEventFilter(MouseEvent.MOUSE_DRAGGED, nodeGestures.getOnMouseDraggedEventHandler());
-
-        canvas.getChildren().addAll(label1, label2, label3, circle1, rect1);
+//        canvas.getChildren().addAll(label1, label2, label3, circle1, rect1);
 
         group.getChildren().add(canvas);
 
@@ -81,14 +84,14 @@ public class ZoomAndScrollApplication extends Application {
         Scene scene = new Scene(group, 1024, 768);
 
         SceneGestures sceneGestures = new SceneGestures(canvas);
-        scene.addEventFilter(MouseEvent.MOUSE_PRESSED, sceneGestures.getOnMousePressedEventHandler());
-        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, sceneGestures.getOnMouseDraggedEventHandler());
+//        scene.addEventFilter(MouseEvent.MOUSE_PRESSED, sceneGestures.getOnMousePressedEventHandler());
+//        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, sceneGestures.getOnMouseDraggedEventHandler());
         scene.addEventFilter(ScrollEvent.ANY, sceneGestures.getOnScrollEventHandler());
 
         stage.setScene(scene);
         stage.show();
 
-        canvas.addGrid();
+//        canvas.addGrid();
 
     }
 }
